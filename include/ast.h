f50fc05a3f2deab54ebbfc2a6442448f489d3a69
florianpeter874-wq <florianpeter874@gmail.com>
2026-09-14 06:17:11 +0200

    Enhance AST structure with new node types and properties

--- include/ast.h
+++ include/ast.h
@@ -6,24 +6,40 @@ typedef enum {
     AST_FUNCTION,
     AST_BLOCK,
     AST_PRINT,
+    AST_RETURN,
     AST_STRING,
 
     AST_IF,
     AST_ELSE,
+    AST_WHILE,
     AST_BINARY,
+    AST_UNARY,
     AST_NUMBER,
 
     AST_ENTRY_POINT,
     AST_CALL,
+    AST_INPUT,
 
     AST_VARIABLE_DECL,
-    AST_VARIABLE_REF
+    AST_VARIABLE_REF,
+    AST_ASSIGNMENT
 } ASTNodeType;
+
+typedef enum {
+    NOVA_TYPE_VOID,
+    NOVA_TYPE_INT,
+    NOVA_TYPE_STR,
+    NOVA_TYPE_FILE
+} NovaType;
 
 typedef struct ASTNode ASTNode;
 
 struct ASTNode {
     ASTNodeType type;
+
+    NovaType data_type;
+
+    int is_array;
 
     char *value;
 
