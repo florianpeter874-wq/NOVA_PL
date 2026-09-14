8c054cb9d8eb75dfdf5e30ff885818ebbc13d6e4
florianpeter874-wq <florianpeter874@gmail.com>
2026-09-14 06:17:44 +0200

    Add debug parameter to compiler_compile function

--- include/compiler.h
+++ include/compiler.h
@@ -1,8 +1,14 @@
+
 #ifndef NOVA_COMPILER_H
 #define NOVA_COMPILER_H
 
 #include "ast.h"
 
-int compiler_compile(ASTNode *root, const char *output_file);
+int compiler_compile(
+    ASTNode *root,
+    const char *output_file,
+    int debug
+);
 
 #endif
+
